4e685ed485e54364484371bd57c72619d7b8e4fb
seth skinner <sethwskinner@gmail.com>
2020-09-09 00:38:19 -0500

    fixing error in spelling in routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -8,7 +8,7 @@ router.get('/', (req, res) => {
 
 });
 
-router.get('/excercise', (req, res) => {
+router.get('/exercise', (req, res) => {
 
     res.sendFile(path.join(__dirname, '../public/exercise.html'));
 
